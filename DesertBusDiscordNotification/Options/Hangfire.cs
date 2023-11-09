c9e39a8810e62e41dda74d61e8b97b51d57f35da
Vincent <vincent@mahn.ke>
2023-11-09 06:30:49 +0100

    feat: Adds cron job to refresh status of giveaways

---
+++ DesertBusDiscordNotification/Options/Hangfire.cs
@@ -0,0 +1,6 @@
+namespace DesertBusDiscordNotification.Options;
+
+internal class Hangfire
+{
+    public required string Password { get; set; }
+}
--- DesertBusDiscordNotification/Program.cs
+++ DesertBusDiscordNotification/Program.cs
@@ -4,7 +4,10 @@ using DesertBusDiscordNotification.Client;
 using DesertBusDiscordNotification.HealthChecks;
 using DesertBusDiscordNotification.Services;
 using DesertBusDiscordNotification.Services.Sender;
+using Hangfire;
+using Hangfire.Dashboard.BasicAuthorization;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
 
 CultureInfo.CurrentCulture = new CultureInfo("en-US");
 CultureInfo.CurrentUICulture = new CultureInfo("en-US");
@@ -18,7 +21,6 @@ builder.Configuration
     
 builder.Services
     .AddHttpClient<IDesertBusAPIClient, DesertBusAPIClient>();
-    
 
 builder.Services
     .AddHealthChecks()
@@ -30,11 +32,20 @@ builder.Services
     ));
 
 builder.Services
-    .Configure<DesertBusDiscordNotification.Options.Discord>(builder.Configuration.GetSection(nameof(DesertBusDiscordNotification.Options.Discord)));
+    .Configure<DesertBusDiscordNotification.Options.Discord>(builder.Configuration.GetSection(nameof(DesertBusDiscordNotification.Options.Discord)))
+    .Configure<DesertBusDiscordNotification.Options.Hangfire>(builder.Configuration.GetSection(nameof(DesertBusDiscordNotification.Options.Hangfire)));
 
 builder.Services
     .AddSingleton<ISender, DiscordSender>()
     .AddSingleton<GiveawayTracker>();
+
+GlobalConfiguration.Configuration.UseInMemoryStorage();
+builder.Services.AddHangfireServer();
+builder.Services.AddHangfire((_, configuration) =>
+{
+    configuration.UseSimpleAssemblyNameTypeSerializer();
+    configuration.UseRecommendedSerializerSettings();
+});
 
 var app = builder.Build();
 app.MapGet("/", () => "OK");
@@ -44,8 +55,34 @@ app.MapGet("/send" , () =>
     app.Services
         .GetRequiredService<ISender>()
         .SendAsync("test", 1.23m, "https://desertbus.org/_nuxt/1630da356cd42da9eff8d517e002743b.png").Wait();
-}); 
+});
 app.MapHealthChecks("/health");
+
+var options = new DashboardOptions
+{
+    Authorization = new[]
+    {
+        new BasicAuthAuthorizationFilter(new BasicAuthAuthorizationFilterOptions
+        {
+            SslRedirect = false,
+            RequireSsl = false,
+            LoginCaseSensitive = true,
+            Users = new []
+            {
+                new BasicAuthAuthorizationUser
+                {
+                    Login = "admin",
+                    PasswordClear = app.Services.GetRequiredService<IOptions<DesertBusDiscordNotification.Options.Hangfire>>().Value.Password
+                }
+            }
+        })
+    }
+};
+
+app.UseHangfireDashboard("/hangfire", options);
+RecurringJob.AddOrUpdate<GiveawayTracker>(nameof(GiveawayTracker), x => x.UpdateAsync(CancellationToken.None), Cron.Minutely());
+RecurringJob.TriggerJob(nameof(GiveawayTracker));
+
 app.Run();
 
 namespace DesertBusDiscordNotification
